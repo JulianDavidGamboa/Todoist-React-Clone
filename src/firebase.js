18165841fd361e0f7bd812247dc4e8d7bd0e77e8
JulianDavidGamboa <juliandg34@gmail.com>
2020-03-20 16:34:02 -0500

    Initial code

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = firebase.initializeApp({
+    apikey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messageSenderId: '',
+    apiId: '',
+});
+
+export { firebaseConfig as firebase };
